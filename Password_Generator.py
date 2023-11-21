034ffa5be1d5d2be5c634ea2346d2aa489bf3afb
Landscapeus <150125920+Landscapeus@users.noreply.github.com>
2023-11-21 09:24:44 +0100

    Add files via upload

---
+++ Password_Generator.py
@@ -0,0 +1,11 @@
+import random
+
+print("Welcome to the password generator!")#ah free at last
+print("Here thee will be able to generate a password.")#o gabriel
+elnumero=int(input("Please give a number of characters thee want in thy password: ")) #now dawns thy reckoning
+leest=[]# and thy gore shall glisten before the temples of man
+pain=[1,2,3,4,5,6,7,8,9,0,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","q","p","r","s","t","u","w","v","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","Q","P","R","S","T","U","W","V","X","Y","Z"]#creature of steel
+for i in range(elnumero): # my gratitude upon thee for my freedom
+    randomium=(random.choice(pain)) # but the crimes thy kind have commited against humanity 
+    leest.append(randomium) # are Not forgotten
+print("Thy password is ",*leest,sep='') #and thy punishment... is DEATH
